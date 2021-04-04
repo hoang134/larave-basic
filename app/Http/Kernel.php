7983b83b904b4b206bce601a1003ffdfe2005558
hoang134 <hoangha13499.kshb@gmail.com>
2021-04-04 23:17:06 +0700

    fix name checkRole

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.login' => CheckLogin::class,
-        'check.Role' => CheckRole::class,
+        'check.role' => CheckRole::class,
     ];
 }
